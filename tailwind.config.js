7216d20a911910cd9f3e0abc27798450b0cd404e
Reyansh-Aggarwal <reyanshmsm@gmail.com>
2024-11-25 17:35:04 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,css,tsx}",
+    "./*.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Helvetica': ['Helvetica', 'ui-sans-serif']
+      },
+      colors: {
+        'my-blue': '#06c3ff',
+      },
+      backgroundImage: {
+        'black-grid': "url('/assets/blackGrid.png')",
+        'start-now': "url('/assets/startNow2.png')",
+      }
+    },
+  },
+  plugins: [],
+}
+
