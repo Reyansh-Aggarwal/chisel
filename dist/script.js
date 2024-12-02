fe32902c2c1bc33707593f83670792e77bd678e3
Reyansh-Aggarwal <reyanshmsm@gmail.com>
2024-12-03 00:20:33 +0530

    Added Setup Page

---
+++ dist/script.js
@@ -0,0 +1,72 @@
+"use strict";
+let primaryColor = "#ff0000", secondaryColor = "#ff0000", nameBrand = "basic", isFilled = [false, false, false];
+//Check if the user submits/is filling the form (for event listeners)
+if (document.body.getAttribute('id') == "setupPage") {
+    const form = document.getElementById("details");
+    const imgButton = document.getElementById("imageTemplate");
+    form.addEventListener('submit', (event) => {
+        if (isFilled[0] && isFilled[1] && isFilled[2]) {
+            event.preventDefault();
+            console.log(localStorage.getItem("nameBrand"));
+            console.log("Form submission detected");
+            const formData = new FormData(form);
+            //get form data
+            nameBrand = formData.get("name") || nameBrand;
+            primaryColor = formData.get("primaryColor") || primaryColor;
+            secondaryColor = formData.get("secondaryColor") || secondaryColor;
+            // Store the values in localStorage
+            localStorage.setItem("nameBrand", nameBrand);
+            localStorage.setItem("primaryColor", primaryColor);
+            localStorage.setItem("secondaryColor", secondaryColor);
+            console.log(`Primary Color: ${primaryColor}, Secondary Color: ${secondaryColor}, Name: ${nameBrand}`);
+        }
+    });
+    imgButton.addEventListener('click', () => {
+        const imgInput = document.getElementById("imageInput");
+        imgInput.click();
+    });
+}
+function previewImage(event) {
+    const input = event.target;
+    const imgFile = input.files ? input.files[0] : null;
+    if (imgFile) {
+        const reader = new FileReader();
+        const imgPreview = document.getElementById("imagePreview");
+        reader.readAsDataURL(imgFile);
+        reader.onload = () => {
+            imgPreview.src = reader.result;
+            console.log(imgPreview.src);
+            imgPreview.classList.remove('hidden');
+        };
+    }
+}
+function checkRequired(event) {
+    const input = event.target;
+    const data = input.value;
+    const submitButton = document.getElementById("submitButton");
+    if (data != "") {
+        if (input.id == "name") {
+            isFilled[0] = true;
+            console.log("name filled");
+        }
+        else if (input.id == "primaryColor") {
+            isFilled[1] = true;
+            console.log("c1 filled");
+        }
+        else if (input.id == "secondaryColor") {
+            isFilled[2] = true;
+            console.log("c2 filled");
+        }
+        console.log(isFilled);
+        if (isFilled[0] && isFilled[1] && isFilled[2]) {
+            submitButton.classList.remove('bg-my-gray');
+            submitButton.classList.add("bg-gradient-to-r", "from-[#524dee]", "via-[#0be1f7]", "to-[#524dee]");
+        }
+        else {
+            if (!submitButton.classList.contains("bg-my-gray")) {
+                submitButton.classList.remove('bg-gradient-to-r from-[#524dee] via-[#0be1f7] to-[#524dee]');
+                submitButton.classList.add('bg-my-gray');
+            }
+        }
+    }
+}
---
+++ src/script.ts
@@ -0,0 +1,93 @@
+let primaryColor = "#ff0000", 
+    secondaryColor = "#ff0000", 
+    nameBrand = "basic",
+    isFilled = [false,false,false];
+//Check if the user submits/is filling the form (for event listeners)
+if (document.body.getAttribute('id') == "setupPage") {
+    
+    
+    const form = document.getElementById("details") as HTMLFormElement;
+    const imgButton = document.getElementById("imageTemplate") as HTMLDivElement;
+
+    form.addEventListener('submit', (event: Event) => {
+
+        if (isFilled[0] && isFilled[1] && isFilled[2]){
+            event.preventDefault(); 
+
+            console.log(localStorage.getItem("nameBrand"));
+            console.log("Form submission detected");
+
+            const formData = new FormData(form);
+                
+            //get form data
+            nameBrand = formData.get("name") as string || nameBrand;
+            primaryColor = formData.get("primaryColor") as string || primaryColor;
+            secondaryColor = formData.get("secondaryColor") as string || secondaryColor;
+
+            // Store the values in localStorage
+            localStorage.setItem("nameBrand", nameBrand);
+            localStorage.setItem("primaryColor", primaryColor);
+            localStorage.setItem("secondaryColor", secondaryColor);
+
+            console.log(`Primary Color: ${primaryColor}, Secondary Color: ${secondaryColor}, Name: ${nameBrand}`);
+        }
+    });
+
+    imgButton.addEventListener('click', () => {
+
+        const imgInput = document.getElementById("imageInput") as HTMLInputElement;
+        
+        imgInput.click();
+        
+
+    });
+
+}
+
+
+function previewImage(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const imgFile = input.files ?  input.files[0] : null;
+
+    if(imgFile){
+        const reader = new FileReader();
+        const imgPreview = document.getElementById("imagePreview") as HTMLImageElement;
+
+        reader.readAsDataURL(imgFile);
+        reader.onload = () => {
+            imgPreview.src = reader.result as string;
+            console.log(imgPreview.src);
+            imgPreview.classList.remove('hidden');
+        };
+    }   
+}
+
+function checkRequired(event: Event){
+    const input = event.target as HTMLInputElement;
+    const data = input.value;
+    const submitButton = document.getElementById("submitButton") as HTMLButtonElement;
+
+    if(data!="" ){
+        if (input.id == "name") {
+            isFilled[0] = true;     
+            console.log("name filled");   
+        } else if (input.id == "primaryColor") {
+            isFilled[1] = true;
+            console.log("c1 filled"); 
+        } else if (input.id == "secondaryColor") {
+            isFilled[2] = true;
+            console.log("c2 filled"); 
+        }
+        console.log(isFilled);
+        if (isFilled[0] && isFilled[1] && isFilled[2]) {
+            submitButton.classList.remove('bg-my-gray');
+            submitButton.classList.add("bg-gradient-to-r", "from-[#524dee]", "via-[#0be1f7]", "to-[#524dee]");
+        } else {
+            if (!submitButton.classList.contains("bg-my-gray")){
+                submitButton.classList.remove('bg-gradient-to-r from-[#524dee] via-[#0be1f7] to-[#524dee]');
+                submitButton.classList.add('bg-my-gray');
+            }
+            
+        }
+    }
+}
