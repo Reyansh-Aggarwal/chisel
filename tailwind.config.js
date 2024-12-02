762898f1ea52bd400d4faa1c8dad3cb791145f5b
Reyansh-Aggarwal <reyanshmsm@gmail.com>
2024-12-03 00:20:33 +0530

    Added Setup Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts,css,tsx}",
+    "./pages/*.{html,ts}",
     "./*.html"
   ],
   theme: {
     extend: {
       fontFamily: {
-        'Helvetica': ['Helvetica', 'ui-sans-serif']
+        'helvetica-light': ['helvetica-light', 'sans-serif'],
+        'helvetica-roman': ['helvetica-roman', 'sans-serif'],
+        'helvetica-bold': ['helvetica-bold', 'sans-serif'],
       },
       colors: {
         'my-blue': '#06c3ff',
+        'my-gray': '#292929'
       },
       backgroundImage: {
         'black-grid': "url('https://reyansh-aggarwal.github.io/chisel/assets/blackGrid.png')",
 
+      },
+      animation: {
+        gradient: 'gradient 3s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        }
+      },
+      letterSpacing: {
+        tightest: '-0.75em'
       }
+
     },
   },
   plugins: [],
