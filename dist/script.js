f6fcfde8d288816f19f71773f267e1c55f67ca05
Reyansh-Aggarwal <reyanshmsm@gmail.com>
2024-12-23 00:19:17 +0530

    fixed problem with image input

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,13 @@
 "use strict";
-let primaryColor = "#ff0000", secondaryColor = "#ff0000", nameBrand = "basic", isFilled = [false, false, false], logoUrl = "";
+let primaryColor = "#ff0000", secondaryColor = "#ff0000", nameBrand = "basic", isFilled = [false, false, false];
 document.addEventListener("DOMContentLoaded", function () {
-    //need always
-    const nameValue = localStorage.getItem("nameBrand");
-    const primaryColorValue = localStorage.getItem("primaryColor");
-    const secondaryColorValue = localStorage.getItem("secondaryColor");
-    const logoImageValue = localStorage.getItem("logoImage");
     if (document.body.getAttribute('id') == "setupPage") {
+        //need always
+        const nameValue = localStorage.getItem("nameBrand");
+        const primaryColorValue = localStorage.getItem("primaryColor");
+        const secondaryColorValue = localStorage.getItem("secondaryColor");
+        const logoImageValue = localStorage.getItem("logoImage");
         const form = document.getElementById("details");
-        const imgButton = document.getElementById("imageTemplate");
         const submitButton = document.getElementById("submitButton");
         //inputs
         const nameInput = document.getElementById("name");
@@ -33,30 +32,30 @@ document.addEventListener("DOMContentLoaded", function () {
             isFilled[2] = true;
         }
         if (logoImageValue) {
-            logoUrl = logoImageValue;
             previewImage(logoImageValue);
             isFilled[3] = true;
         }
-        //-===
-        //submit
+        //===================================================================
         form.addEventListener('submit', (event) => {
             if (nameValue && primaryColorValue || secondaryColorValue) {
                 event.preventDefault();
+                let logoUrl = "";
                 console.log("Form submission detected");
                 const formData = new FormData(form);
+                const reader = new FileReader();
                 //get form data
                 nameBrand = formData.get("name") || nameBrand;
                 primaryColor = formData.get("primaryColor") || primaryColor;
                 secondaryColor = formData.get("secondaryColor") || secondaryColor;
-                const logoImage = formData.get("logo") || logoUrl;
+                const logoImage = imgPreview.src;
                 //image handling
                 if (logoImage) {
-                    localStorage.setItem("logoImage", logoUrl);
+                    localStorage.setItem("logoImage", logoImage);
                     console.log("Uploaded successfully");
                 }
                 //handling window changes seperately
                 if (imgPreview.src != "") {
-                    window.location.href = "../pages/logoPage.html";
+                    //window.location.href = "../pages/logoPage.html";
                 }
                 else if (imgPreview.src == "") {
                     window.location.href = "";
@@ -68,39 +67,40 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.log(`Primary Color: ${primaryColor}, Secondary Color: ${secondaryColor}, Name: ${nameBrand}]`);
             }
         });
-        imgButton.addEventListener('onchange', () => {
-            const imgInput = document.getElementById("imageInput");
-            const imgFile = imgInput.files ? imgInput.files[0] : null;
-            if (imgFile) {
-                const reader = new FileReader();
-                reader.readAsDataURL(imgFile);
-                reader.onload = () => {
-                    previewImage(reader.result);
-                };
-            }
-            //emptying <input> so that the user can upload a new image
-            imgInput.value = "";
-        });
-        function previewImage(logoUrl) {
-            //telling that the image has been uploaded
-            isFilled[3] = true;
-            const prevDiv = document.getElementById("imagePreviewDiv");
-            const imgPreview = document.getElementById("imagePreview");
-            const imgTemplate = document.getElementById("imageTemplate");
-            const imgDiv = document.getElementById("imageDiv");
-            imgPreview.src = logoUrl;
-            //changing classes (tailwind) of divs for styling
-            prevDiv.classList.remove('absolute');
-            imgPreview.classList.remove('hidden');
-            imgTemplate.classList.add('hidden');
-            imgPreview.classList.add('block');
-            imgDiv.classList.add('bg-transparent');
-        }
     }
     if (document.body.getAttribute('id') == "logoPage") {
     }
 });
-//cant put in main if statement because of some reason??
+//cant put in main if statement (DOMContentLoader)
+function previewImage(logoUrl) {
+    //telling that the image has been uploaded
+    console.log("image previewed");
+    const prevDiv = document.getElementById("imagePreviewDiv");
+    const imgPreview = document.getElementById("imagePreview");
+    const imgTemplate = document.getElementById("imageTemplate");
+    const imgDiv = document.getElementById("imageDiv");
+    imgPreview.src = logoUrl;
+    //changing classes (tailwind) of divs for styling
+    prevDiv.classList.remove('absolute');
+    imgPreview.classList.remove('hidden');
+    imgTemplate.classList.add('hidden');
+    imgPreview.classList.add('block');
+    imgDiv.classList.add('bg-transparent');
+}
+function imageInputChange() {
+    const imgInput = document.getElementById("imageInput");
+    const imgFile = imgInput.files ? imgInput.files[0] : null;
+    if (imgFile) {
+        const reader = new FileReader();
+        reader.readAsDataURL(imgFile);
+        reader.onload = () => {
+            previewImage(reader.result);
+        };
+    }
+    console.log("hello");
+    //emptying <input> so that the user can upload a new image
+    imgInput.value = "";
+}
 function checkRequired(event) {
     const input = event.target;
     const data = input.value;
