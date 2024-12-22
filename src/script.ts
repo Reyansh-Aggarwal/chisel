ea13c3cba38d35a185bce179df97470d2748b825
Reyansh-Aggarwal <reyanshmsm@gmail.com>
2024-12-23 00:19:17 +0530

    fixed problem with image input

--- src/script.ts
+++ src/script.ts
@@ -1,30 +1,27 @@
 let primaryColor = "#ff0000", 
     secondaryColor = "#ff0000", 
     nameBrand = "basic",
-    isFilled = [false,false,false],
-    logoUrl = "";
-
-    
+    isFilled = [false,false,false];
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    //need always
-    const nameValue = localStorage.getItem("nameBrand");
-    const primaryColorValue = localStorage.getItem("primaryColor");
-    const secondaryColorValue = localStorage.getItem("secondaryColor");
-    const logoImageValue = localStorage.getItem("logoImage");
+    
 
     if (document.body.getAttribute('id') == "setupPage") {
         
+        //need always
+        const nameValue = localStorage.getItem("nameBrand");
+        const primaryColorValue = localStorage.getItem("primaryColor");
+        const secondaryColorValue = localStorage.getItem("secondaryColor");
+        const logoImageValue = localStorage.getItem("logoImage");
+
         const form = document.getElementById("details") as HTMLFormElement;
-        const imgButton = document.getElementById("imageTemplate") as HTMLDivElement;
         const submitButton = document.getElementById("submitButton") as HTMLButtonElement;
         //inputs
         const nameInput = document.getElementById("name") as HTMLInputElement;
         const primaryColorInput = document.getElementById("primaryColor") as HTMLInputElement;
         const secondaryColorInput = document.getElementById("secondaryColor") as HTMLInputElement;
         const imgPreview = document.getElementById("imagePreview") as HTMLImageElement;
-        
 
         //fill stored values
         
@@ -35,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function () {
             nameInput.value = nameValue;
             makeGradient(submitButton, true);
         }
-
         if (primaryColorValue) {
             primaryColor = primaryColorValue;
             primaryColorInput.value = primaryColorValue;
@@ -47,39 +43,41 @@ document.addEventListener("DOMContentLoaded", function () {
             isFilled[2] = true;
         } 
         if (logoImageValue) {
-            logoUrl = logoImageValue;
             previewImage(logoImageValue);
             isFilled[3] = true;
         }
-        //-===
+        //===================================================================
 
-        //submit
         form.addEventListener('submit', (event: Event) => {
     
             if (nameValue && primaryColorValue || secondaryColorValue) {
                 event.preventDefault(); 
-    
+
+                let logoUrl = "";
+
                 console.log("Form submission detected");
                 const formData = new FormData(form);
                     
+                const reader = new FileReader();
                 //get form data
                 nameBrand = formData.get("name") as string || nameBrand;
                 primaryColor = formData.get("primaryColor") as string || primaryColor;
                 secondaryColor = formData.get("secondaryColor") as string || secondaryColor;
-                const logoImage = formData.get("logo") as File || logoUrl;
+                const logoImage = imgPreview.src;
 
                 //image handling
                 if(logoImage){
-                    localStorage.setItem("logoImage", logoUrl);
+                    localStorage.setItem("logoImage", logoImage);
                     console.log("Uploaded successfully");
                 } 
                 
                 //handling window changes seperately
                 if(imgPreview.src != ""){
-                    window.location.href = "../pages/logoPage.html";
+                    //window.location.href = "../pages/logoPage.html";
                 } else if (imgPreview.src == "") {
                     window.location.href = "";
                 }
+
                 // Store the values in localStorage
                 localStorage.setItem("nameBrand", nameBrand);
                 localStorage.setItem("primaryColor", primaryColor);
@@ -88,45 +86,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.log(`Primary Color: ${primaryColor}, Secondary Color: ${secondaryColor}, Name: ${nameBrand}]`);
             }
         });
-    
-        imgButton.addEventListener('onchange', () => {
-            const imgInput = document.getElementById("imageInput") as HTMLInputElement;
-            const imgFile = imgInput.files ?  imgInput.files[0] : null;
-            if(imgFile){
-                const reader = new FileReader();
-                reader.readAsDataURL(imgFile);
-                reader.onload = () => {
-                    previewImage(reader.result as string);
-                };
-            }
 
-            //emptying <input> so that the user can upload a new image
-            imgInput.value = "";
-            
-        });
-
-        function previewImage(logoUrl : string) {
-            
-            //telling that the image has been uploaded
-            isFilled[3] = true;
-                
-            const prevDiv = document.getElementById("imagePreviewDiv") as HTMLDivElement;
-            const imgPreview = document.getElementById("imagePreview") as HTMLImageElement;
-            const imgTemplate = document.getElementById("imageTemplate") as HTMLDivElement;
-            const imgDiv = document.getElementById("imageDiv") as HTMLDivElement;
         
-            imgPreview.src = logoUrl;
-                
-            //changing classes (tailwind) of divs for styling
-            prevDiv.classList.remove('absolute');
-            imgPreview.classList.remove('hidden');
-            imgTemplate.classList.add('hidden');
-            imgPreview.classList.add('block');
-            imgDiv.classList.add('bg-transparent');
-
-            
-
-        }
     }
     
     if (document.body.getAttribute('id') == "logoPage"){
@@ -136,7 +97,43 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-//cant put in main if statement because of some reason??
+//cant put in main if statement (DOMContentLoader)
+function previewImage(logoUrl : string) {           
+    //telling that the image has been uploaded
+
+    console.log("image previewed");
+    const prevDiv = document.getElementById("imagePreviewDiv") as HTMLDivElement;
+    const imgPreview = document.getElementById("imagePreview") as HTMLImageElement;
+    const imgTemplate = document.getElementById("imageTemplate") as HTMLDivElement;
+    const imgDiv = document.getElementById("imageDiv") as HTMLDivElement;
+
+    imgPreview.src = logoUrl;
+        
+    //changing classes (tailwind) of divs for styling
+    prevDiv.classList.remove('absolute');
+    imgPreview.classList.remove('hidden');
+    imgTemplate.classList.add('hidden');
+    imgPreview.classList.add('block');
+    imgDiv.classList.add('bg-transparent');
+
+}
+
+function imageInputChange(){
+    const imgInput = document.getElementById("imageInput") as HTMLInputElement;
+    const imgFile = imgInput.files ?  imgInput.files[0] : null;
+    if(imgFile){
+        const reader = new FileReader();
+        reader.readAsDataURL(imgFile);
+        reader.onload = () => {
+            previewImage(reader.result as string);
+        };
+    }
+    console.log("hello");
+    //emptying <input> so that the user can upload a new image
+    imgInput.value = "";
+    
+}
+
 function checkRequired(event: Event){
     const input = event.target as HTMLInputElement;
     const data = input.value;
